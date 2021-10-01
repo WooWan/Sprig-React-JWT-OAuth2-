56280be13d139d29681040fadc6691b86e8587dd
wooho <woohobi@gmail.com>
2021-10-02 01:28:32 +0900

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SocialLogin from "./components/SocialLogin";
+
+function App() {
+  return (
+    <div className="App">
+      <SocialLogin>
+
+      </SocialLogin>
+    </div>
+  );
+}
+
+export default App;
