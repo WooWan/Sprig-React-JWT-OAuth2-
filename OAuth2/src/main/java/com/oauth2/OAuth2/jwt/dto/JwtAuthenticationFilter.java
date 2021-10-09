e88f9db6331558d471154c013271c5acbe90561b
wooho <woohobi@gmail.com>
2021-10-09 23:21:34 +0900

    jwt 필터 추가

--- OAuth2/src/main/java/com/oauth2/OAuth2/jwt/dto/JwtAuthenticationFilter.java
+++ OAuth2/src/main/java/com/oauth2/OAuth2/jwt/dto/JwtAuthenticationFilter.java
@@ -9,10 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-    }
-}
+//public class JwtAuthenticationFilter extends OncePerRequestFilter{
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//
+//    }
+//}
