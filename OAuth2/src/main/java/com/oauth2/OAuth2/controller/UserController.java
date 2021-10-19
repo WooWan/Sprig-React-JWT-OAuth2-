4da2ca20873854d1e1433f6b1adc033f3c6341e0
wooho <woohobi@gmail.com>
2021-10-20 00:39:59 +0900

    user controller 수정

--- OAuth2/src/main/java/com/oauth2/OAuth2/controller/UserController.java
+++ OAuth2/src/main/java/com/oauth2/OAuth2/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.oauth2.OAuth2.controller;
 
+import com.oauth2.OAuth2.domain.User;
+import com.oauth2.OAuth2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -7,11 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
+    private final UserRepository userRepository;
 
-
+    @GetMapping("/api/user")
+    public List<User> userList() {
+        List<User> userList = userRepository.findAll();
+        log.info("users : {}" , userList);
+        return userList;
+    }
 }
