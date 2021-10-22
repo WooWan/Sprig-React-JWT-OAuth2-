c8043a45e46d84c11501bade4998205a6db30d85
wooho <woohobi@gmail.com>
2021-10-22 21:27:24 +0900

    refresh token 설정 추가

--- frontend/src/components/OAuth2RedirectHandler.jsx
+++ frontend/src/components/OAuth2RedirectHandler.jsx
@@ -3,13 +3,14 @@ import qs from "qs";
 import {Redirect} from "react-router-dom";
 
 const OAuth2RedirectHandler = ({location}) => {
-
     const query  = qs.parse(location.search ,{
         ignoreQueryPrefix: true
     })
+    const refreshToken = query.refreshToken
     const token = query.token
     if (token) {
         localStorage.setItem("ACCESS_TOKEN", token)
+        localStorage.setItem("REFRESH_TOKEN", refreshToken)
         return <Redirect to= "/user" />
     }
 
--- frontend/src/service/server.js
+++ frontend/src/service/server.js
@@ -8,8 +8,10 @@ class Server {
         this.server.interceptors.request.use(
             config =>{
                 const token = localStorage.getItem("ACCESS_TOKEN");
+                const refreshToken = localStorage.getItem("REFRESH_TOKEN");
                 if (token) {
                     config.headers['Authorization'] = 'Bearer ' + token;
+                    config.headers['RefreshToken'] = refreshToken;
                 }
                 config.headers["Content-Type"] = "application/json; charset=utf-8";
                 console.log(config)
