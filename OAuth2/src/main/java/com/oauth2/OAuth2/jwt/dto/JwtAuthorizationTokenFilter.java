2678c5db8008345c04ef14621f96c90c2727d234
wooho <woohobi@gmail.com>
2021-10-25 23:44:35 +0900

    refresh token 기한 만료 예외 처리

--- OAuth2/src/main/java/com/oauth2/OAuth2/jwt/dto/JwtAuthorizationTokenFilter.java
+++ OAuth2/src/main/java/com/oauth2/OAuth2/jwt/dto/JwtAuthorizationTokenFilter.java
@@ -1,6 +1,8 @@
 package com.oauth2.OAuth2.jwt.dto;
 
+import com.oauth2.OAuth2.utils.RedisUtil;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -20,16 +23,34 @@ import java.io.IOException;
 public class JwtAuthorizationTokenFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
+    private final RedisUtil redisUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorization = request.getHeader("Authorization");
-        String jwtToken = getJwtFromRequest(authorization);
-        log.info("jwttoken :{}" , jwtToken);
-        if (StringUtils.hasText(jwtToken) && tokenProvider.validateToken(jwtToken)) {
-            System.out.println("jwtToken = " + jwtToken);
-            Claims body = tokenProvider.getClaimsFromToken(jwtToken);
-            log.info("claims body: {}", body);
+        try {
+            String jwtToken = getJwtFromRequest(authorization);
+            if (StringUtils.hasText(jwtToken) && tokenProvider.validateToken(jwtToken)) {
+                System.out.println("jwtToken = " + jwtToken);
+                Claims body = tokenProvider.getClaimsFromToken(jwtToken);
+                log.info("claims body: {}", body);
+            }
+        } catch (ExpiredJwtException jwtException) {
+//            access token이 만료되었다면,
+            String refreshHeader = request.getHeader("RefreshToken");
+            String refreshToken = getJwtFromRequest(refreshHeader);
+            Optional<Claims> optionalRefreshBody = Optional.empty();
+            try {
+                optionalRefreshBody = Optional.of(tokenProvider.getClaimsFromToken(refreshToken));
+            } catch (ExpiredJwtException expiredRefreshTokenException) {
+                log.info("refresh Token :{} ", refreshToken);
+            }
+            if (optionalRefreshBody.isPresent()) {
+                tokenProvider.createAccessToken();
+            }
+            String redisData = redisUtil.getData(refreshToken);
+//            refresh token 이 유효하다면,access token 재발급
+//            tokenProvider.createToken()
         }
         filterChain.doFilter(request, response);
     }
